c7a1d6d83fa324f78bb2c3fb07e376bbfef1bd34
TheLastVelo <jarellchinn44@gmail.com>
2023-08-01 22:21:09 -0700

    routes complete

--- routes/notes.js
+++ routes/notes.js
@@ -4,8 +4,8 @@ const fs = require("fs");
 // import unique identifier package
 
 // - `/api/notes` should read the `db.json` file and return all saved notes as JSON.
-router.get("/api/notes", (req, res) => {
-  fs.readFile("db.json", "utf8", (err, data) => {
+router.get("/", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) {
       console.error(err);
       return res
@@ -13,6 +13,7 @@ router.get("/api/notes", (req, res) => {
         .json({ error: "Something went wrong reading the notes. " });
     }
     const notes = JSON.parse(data);
+    res.json(notes);
   });
 
   // READ db.json
@@ -20,7 +21,7 @@ router.get("/api/notes", (req, res) => {
 // `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json`
 
 // unique identifier package
-router.post("/api/notes", (req, res) => {
+router.post("/", (req, res) => {
   // Reading the file
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) {
@@ -36,22 +37,23 @@ router.post("/api/notes", (req, res) => {
     newNote.id = uuidv4();
     // pushing new note to array
     notes.push(newNote);
-  });
-  // write file
-  fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: "Was not able to save note" });
-    }
-    res.json(newNote);
+
+    // write file
+    fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Was not able to save note" });
+      }
+      res.json(newNote);
+    });
   });
 });
 
-router.delete("", (req, res) => {
-  // Read file
-  // parse into array
-  // filter out note with the Id that would be passed in
-  // write to file
-});
+// router.delete("", (req, res) => {
+// Read file
+// parse into array
+// filter out note with the Id that would be passed in
+// write to file
+// });
 
 module.exports = router;
